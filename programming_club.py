e0208c32205dad4135f962f39c685e61e7f184e1
Xander Clair <xander@Johns-Mac-mini.hsd1.or.comcast.net>
2016-10-12 21:31:48 -0700

    Initial commit

---
+++ programming_club.py
@@ -0,0 +1,26 @@
+print ""
+
+class Enemy:
+	def __init__(self, name, size, color, damage):
+		self.name = name
+		self.size = size
+		self.color = color
+		self.damage = damage
+	def do_damage(self):
+		print self.name + " does " + str(self.damage) + " damage."
+
+class Goblin (Enemy):
+	def steal(self):
+		print self.name + " steals all of your gold."
+		
+class BadBaker (Enemy):
+	def bake_cookies(self):
+		print self.name + " bakes cookies, but they're raw!"
+		
+george = Goblin("George", 50, "green", 20)
+
+george.do_damage()
+
+xander = BadBaker("Xander", 30, "white", -20)
+
+xander.bake_cookies()
